c4a6a97350745db3bdb8e23a3e89ef53b98a8689
Alexander Sukharev <70752669+Perseus-hub@users.noreply.github.com>
2024-01-15 00:48:30 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
+import logging
 
 ua = UserAgent()
 URL = "https://www.babla.ru/%D1%81%D0%BF%D1%80%D1%8F%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F/%D0%B8%D1%81%D0%BF%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9/"
@@ -12,7 +13,7 @@ def parse(mood, tense, word):
     r = requests.get(URL + word, headers=headers)
 
     result_list = {}
-
+    logging.info(mood,tense,word, r.status_code)
     if r.status_code == 200:
         soup = bs(r.text, "html.parser")
         all_tenses = soup.find_all('div', class_="conj-tense-wrapper")
